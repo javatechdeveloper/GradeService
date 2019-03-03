3ab59fdf07f5f97c712015986025928d7f9e9595
esakk <esakk@192.168.0.20>
2019-03-02 21:08:09 -0500

    gradecontroller

--- Grade/src/main/java/com/javatechdeveloper/grade/v1/service/endpoints/GradeControllerService.java
+++ Grade/src/main/java/com/javatechdeveloper/grade/v1/service/endpoints/GradeControllerService.java
@@ -1,14 +1,18 @@
-package com.javatechdeveloper.grade.v1.service;
+package com.javatechdeveloper.grade.v1.service.endpoints;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javatechdeveloper.grade.v1.cargo.Staff;
 import com.javatechdeveloper.grade.v1.cargo.Student;
 
 
-
+@RestController
+@RequestMapping(value="/grade", produces= MediaType.APPLICATION_JSON_VALUE, consumes= MediaType.APPLICATION_JSON_VALUE)
 public class GradeControllerService {
 
 	@GetMapping
